759c364b412184cc94dfe8c8a61650221bc191af
Nickolas Maia de Araujo <99838582+nickolss@users.noreply.github.com>
2025-03-04 18:34:43 -0300

    fsadfd

--- setup.sh
+++ setup.sh
@@ -29,62 +29,47 @@ _/j  L l\_!  _//^---^\\_
 
 
 
-lsblk
-read -p "Digite o disco onde deseja instalar (exemplo: /dev/sda ou /dev/nvme0n1): " DISK
-
-echo "O disco selecionado foi: $DISK. Isso apagará TODOS os dados!"
-read -p "Tem certeza? (s/n): " CONFIRM
-if [[ "$CONFIRM" != "s" ]]; then
-    echo "Instalação cancelada!"
-    exit 1
-fi
-
 loadkeys br-abnt2
 
-parted -s $DISK mklabel gpt
-parted -s ${DISK} mkpart primary fat32 0% 1GB
-parted -s ${DISK} set 1 boot on
-parted -s ${DISK} mkpart primary btrfs 1GB 51GB
-parted -s ${DISK} mkpart primary btrfs 51GB 100%
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sda mkpart primary fat32 0% 1GB
+parted -s /dev/sda set 1 boot on
+parted -s /dev/sda mkpart primary btrfs 1GB 51GB
+parted -s /dev/sda mkpart primary btrfs 51GB 100%
 
-mkfs.fat -F32 ${DISK}1        
-mkfs.btrfs ${DISK}2            
-mkfs.btrfs ${DISK}3           
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs /dev/sda2
+mkfs.btrfs /dev/sda3
 
+mount /dev/sda2 /mnt
+mkdir -p /mnt/home /mnt/boot/efi
 
-mount ${DISK}2 /mnt
-mkdir -p /mnt/boot/efi /mnt/home
+mount /dev/sda3 /mnt/home
+mount /dev/sda1 /mnt/boot/efi
 
-mount ${DISK}3 /mnt/home
-mount ${DISK}1 /mnt/boot/efi
-
-pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd 
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-
 arch-chroot /mnt <<EOF
 
-
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
 hwclock --systohc
 
-
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+
 locale-gen
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf  
 
+echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
 
-echo "root:arch" | sudo chpasswd
+echo "root:Senha123$" | sudo chpasswd
 
 useradd -m -g users -G wheel, storage, power -s /bin/bash kazz
 
-echo "arch:arch" | sudo chpasswd
+echo "usuario:Senha123$" | sudo chpasswd
 
-pacman -S dosfstools os-prober mtools network-manager-applet wpa_supplicant dialog --noconfirm
-
-pacman -S grub efibootmgr
+pacman -S dosfstools os-prober mtools network-manager-applet wpa_supplicant dialog grub efibootmgr --noconfirm
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
 
@@ -92,18 +77,16 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S xorg-server xorg-xinit xorg-apps mesa --noconfirm
 
-sudo pacman -S xf86-video-amdgpu
-sudo pacman -S xf86-video-intel
-sudo pacman -S nvidia nvidia-settings
-sudo pacman -S virtualbox-guest-utils
+sudo pacman -S xf86-video-amdgpu --noconfirm
+sudo pacman -S xf86-video-intel --noconfirm
+sudo pacman -S nvidia nvidia-settings --noconfirm
+sudo pacman -S virtualbox-guest-utils --noconfirm
 
 pacman -S gnome-extra gnome-terminal --noconfirm
-pacman -S plasma-desktop konsole --noconfirm 
+pacman -S plasma-desktop konsole --noconfirm
 pacman -S gdm --noconfirm
 
 systemctl enable gdm
-systemctl enable NetworkManager 
+systemctl enable NetworkManager
 
 EOF
-
-reboot
