8a38359188684480729b018b9ad2d7c79e67f652
Nickolas Maia de Araujo <99838582+nickolss@users.noreply.github.com>
2025-03-04 18:50:50 -0300

    feat: removed echos

--- setup.sh
+++ setup.sh
@@ -42,14 +42,11 @@ if [[ "$CONFIRM" != "s" ]]; then
     exit 1
 fi
 
-
 read -p "Digite o nome do usuário: " USERNAME
 
-echo "===== Atualizando relógio ====="
 timedatectl set-ntp true
 
 
-echo "===== Particionando Disco ====="
 parted -s $DISK mklabel gpt
 
 # Partição EFI
@@ -67,8 +64,6 @@ mkfs.btrfs ${DISK}2
 mkfs.btrfs ${DISK}3           
 
 
-
-echo "===== Montando Partições ====="
 mount ${DISK}3 /mnt
 mkdir -p /mnt/home
 mkdir -p /mnt/boot
@@ -81,28 +76,23 @@ mount ${DISK}1 /mnt/boot/efi
 
 
 
-echo "===== Instalando Base do Sistema ====="
 pacstrap /mnt base base-devel linux-zen linux-firmware nano dhcpcd 
 
 
 
-echo "===== Gerando fstab ====="
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
 
 
-echo "===== Entrando no Sistema Instalado ====="
 arch-chroot /mnt
 
 
 
-echo "===== Configurando Fuso Horário ====="
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 
 
 
-echo "===== Configurando Locale ====="
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
@@ -110,59 +100,46 @@ echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 
 
 
-echo "===== Configurando Teclado ====="
 echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
 
 
 
-echo "===== Definindo Senha do Root ====="
 echo "Defina uma senha para o root:"
 passwd
 
 
-echo "===== Criando Usuário Final ====="
 useradd -m -g users -G wheel,storage,power -s /bin/bash $USERNAME
 
 echo "Defina uma senha para o usuário $USERNAME:"
 passwd $USERNAME
 
 
-echo "===== Instalando Ferramentas para Dual Boot e Internet ====="
 pacman -S dosfstools os-prober mtools network-manager-applet wpa_supplicant dialog
 
-echo "===== Instalando Ferramentas para Configurar o Grub ====="
 pacman -S grub efibootmgr
 
 
 
-echo "===== Instalando GRUB ====="
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arcg_grub --recheck
 
-echo "===== Configurando Grub ====="
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "===== Configurando Sudo ====="
 echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
-echo "===== Instalando Interface Grafica ====="
 pacman -S xorg-server xorg-xinit xorg-apps mesa
 
-echo "===== Subir Drive de Video ====="
 sudo pacman -S xf86-video-amdgpu
 sudo pacman -S xf86-video-intel
 sudo pacman -S nvidia nvidia-settings
 sudo pacman -S virtualbox-guest-utils
 
-echo "===== Instalando Tipo De Interface Grafica ====="
 pacman -S gnome-extra gnome-terminal
 pacman -S plasma-desktop konsole   
 pacman -S gdm 
 
-echo "===== Configurando Interface Grafica e Rede ====="
 systemctl enable gdm
 systemctl enable NetworkManager
 
 
-echo "===== Finalizando Instalação ====="
 exit 
 reboot
