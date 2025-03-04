801e4857488140053885df25d0bf8ba41220778f
Nickolas Maia de Araujo <99838582+nickolss@users.noreply.github.com>
2025-03-04 19:30:15 -0300

    feat: Set values statically

--- setup.sh
+++ setup.sh
@@ -31,76 +31,50 @@ _/j  L l\_!  _//^---^\\_
 "
 
 
-lsblk
-read -p "Digite o disco onde deseja instalar (exemplo: /dev/sda ou /dev/nvme0n1): " DISK
-
-echo "O disco selecionado foi: $DISK. Isso apagará TODOS os dados!"
-read -p "Tem certeza? (s/n): " CONFIRM
-if [[ "$CONFIRM" != "s" ]]; then
-    echo "Instalação cancelada!"
-    exit 1
-fi
-
 loadkeys br-abnt2
 
-timedatectl set-ntp true
+parted -s /dev/sda mklabel gpt
+parted -s /dev/sda mkpart primary fat32 0% 1GB
+parted -s /dev/sda set 1 boot on
+parted -s /dev/sda mkpart primary btrfs 1GB 51GB
+parted -s /dev/sda mkpart primary btrfs 51GB 100%
 
-parted -s $DISK mklabel gpt
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs /dev/sda2
+mkfs.btrfs /dev/sda3
 
-# Partição EFI
-parted -s $DISK mkpart ESP fat32 0% 1GiB
-parted -s $DISK set 1 boot on
-
-# Partição Root
-parted -s $DISK mkpart primary btrfs 1GiB 50GiB  
-
-# Partição Home
-parted -s $DISK mkpart primary btrfs 50GiB 100%  
-
-mkfs.fat -F32 ${DISK}1        
-mkfs.btrfs ${DISK}2            
-mkfs.btrfs ${DISK}3           
-
-
-mount ${DISK}2 /mnt
+mount /dev/sda2 /mnt
 mkdir -p /mnt/home /mnt/boot/efi
 
-mount ${DISK}3 /mnt/home
-mount ${DISK}1 /mnt/boot/efi
+mount /dev/sda3 /mnt/home
+mount /dev/sda1 /mnt/boot/efi
 
-
-pacstrap /mnt base base-devel linux-zen linux-firmware nano dhcpcd 
-
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-
 arch-chroot /mnt <<EOF
 
-
-
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
 hwclock --systohc
 
-
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+
 locale-gen
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf  
+
 echo KEYMAP=br-abnt2 >> /etc/vconsole.conf
 
+echo "root:Senha123$" | sudo chpasswd
 
-echo "root:arch" | sudo chpasswd
+useradd -m -g users -G wheel, storage, power -s /bin/bash kazz
 
-useradd -m -g users -G wheel, storage, power -s /bin/bash arch
-echo "arch:arch" | sudo chpasswd
+echo "usuario:Senha123$" | sudo chpasswd
 
-pacman -S dosfstools os-prober mtools network-manager-applet wpa_supplicant dialog
-
-pacman -S grub efibootmgr
-
+pacman -S dosfstools os-prober mtools network-manager-applet wpa_supplicant dialog grub efibootmgr --noconfirm
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+
 grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S xorg-server xorg-xinit xorg-apps mesa --noconfirm
@@ -118,5 +92,3 @@ systemctl enable gdm
 systemctl enable NetworkManager
 
 EOF
-
-reboot
